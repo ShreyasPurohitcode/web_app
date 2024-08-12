a8781a649264c1d87413b51c2895666269d62b45
Shreyas Purohit <shreyaspurohit1@gmail.com>
2024-08-12 15:56:32 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Welcome to Shreyas' DevOps Journey!</h1><p>This simple web app is Dockerized and CI/CD automated. Let's keep the cool stuff rolling, one commit at a time!</p>"
+    return "<h1>Welcome to Shreyas' project</p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
